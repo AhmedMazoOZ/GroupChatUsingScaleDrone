f187f4fd3d42507cd9da248c3ac913b2f6524faa
A.azoOZ <ahmedazooz15@gmail.com>
2019-03-16 09:35:11 +0200

    first commit

---
+++ app/src/main/java/com/ahmedazooz/chatapp/MessageViewHolder.java
@@ -0,0 +1,12 @@
+package com.ahmedazooz.chatapp;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class MessageViewHolder {
+
+        public View avatar;
+        public TextView name;
+        public TextView messageBody;
+
+}
